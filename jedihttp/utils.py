2bea773a7a7072b11e2beea0a53629c3f9a4cb23
Cédric Bosdonnat <cedric.bosdonnat@free.fr>
2017-05-15 14:57:52 +0200

    Don't raise an error for missing vendor folder
    When packaging jedihttp in a linux distro, the dependencies in the vendor
    folder aren't shipped because the system version of them is required.
    Only add the vendor folder to the python path if it is present.

--- jedihttp/utils.py
+++ jedihttp/utils.py
@@ -18,6 +18,7 @@ import sys
 def add_vendor_folder_to_sys_path():
     vendor_folder = os.path.join(os.path.dirname(__file__), '..', 'vendor')
 
-    for folder in os.listdir(vendor_folder):
-        sys.path.insert(0, os.path.realpath(os.path.join(vendor_folder,
-                                                         folder)))
+    if os.path.exists(vendor_folder):
+        for folder in os.listdir(vendor_folder):
+            sys.path.insert(0, os.path.realpath(os.path.join(vendor_folder,
+                                                             folder)))
